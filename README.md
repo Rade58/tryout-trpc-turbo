54192cfdbd8bc96eabf3b8d74525573cdfc62966
Rade58 <bajic.rade2@gmail.com>
2023-01-23 19:50:02 +0100

    🦉

--- README.md
+++ README.md
@@ -10,5 +10,19 @@ AND THEN WE CAN
 
 - import module (primarly the router we build) and use it with our express app
 
-- also we can import parts of same module in our client apps (other express app for example, andour react application), so clint can interact with api 
+- also we can import parts of same module in our client apps (other express app for example, andour react application), so clint can interact with api (**I THINK WE NEED AppRouter TYPE**)
+
+# What to do on the client side
+
+<https://trpc.io/docs/react#client-side>
+
+you also need @trpc/server because it is a peer dependancy of @trpc/client
+
+same case here 
+
+>> @trpc/react-query provides a thin wrapper over @tanstack/react-query. It is required as a peer dependency.
+
+
+
+
 
---
+++ apps/my_client/src/utils/TrcpProviders.tsx
@@ -0,0 +1,31 @@
+import { useState, ReactNode, FC } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { trpc } from "./trpc";
+
+const TrpcProviders: FC<{ children: ReactNode }> = ({ children }) => {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() => {
+    return trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: "http://localhost:2021/trpc",
+          // optional
+          // headers() {
+          // return {
+          // authorization: getAuthCookie(),
+          // };
+          // },
+        }),
+      ],
+    });
+  });
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  );
+};
+
+export default TrpcProviders;
