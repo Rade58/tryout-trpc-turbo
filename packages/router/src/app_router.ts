c95e8e1cc96afd402880267f923dce500548c5f9
Rade58 <bajic.rade2@gmail.com>
2023-01-23 13:56:43 +0100

    🦉

--- packages/router/src/app_router.ts
+++ packages/router/src/app_router.ts
@@ -13,7 +13,7 @@ const createContext = async ({
   // THIS FUNCTION WILL "SIMULATE", GETTING THE USER
   // FROM THE DATABASE
   async function getUser() {
-    if (req.headers.authorization !== "Bearer blhblahtrah") {
+    if (req.headers.authorization !== "Bearer abc1245699-sdfdsffd") {
       return null;
     } else {
       return "Shibatoshi";
@@ -34,6 +34,44 @@ type Context = inferAsyncReturnType<typeof createContext>;
 
 const t = initTRPC.context<Context>().create();
 
+// router and public procedure
 const router = t.router;
+const publicProcedure = t.procedure;
+//
+
+// THIS CODE HERE SIMULATES DATBASE I GUESS
+// I JUST DON'T KNOW RIGHT NOW, WHY EVENTS NEED TO BE EMMITED
+
+let id = 0;
+
+const ee = new EventEmitter();
+
+// WE WILL HAVE TWO TYPES OF RECORDS IN THE DATBASE
+//   Post      AND     Message
+const db = {
+  posts: [
+    {
+      id: ++id,
+      title: "Hello shiba",
+    },
+  ],
+  messages: [createMessage("Samoyed is here")],
+};
+
+function createMessage(text: string) {
+  const msg = {
+    id: ++id,
+    text,
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+  };
+
+  // AS I SAID, I DON'T NOW I AM EMMITING THIS MESSAGE HERE
+  ee.emit("newMessage", msg);
+  // BUT WE RE RETURNING MESSAGE OBJECT HERE
+  return msg;
+}
+
+// WE HAVE TWO ROUTES
 
 export {};
